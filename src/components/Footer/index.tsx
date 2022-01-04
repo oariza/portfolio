fa6fc3878284173f735c79f2795770066d4bc240
Luis Omar Ariza Cazarez <61258472+oariza@users.noreply.github.com>
2022-01-04 16:57:22 -0600

    Update copyright year

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import * as Styled from "./styled";
 function Footer() {
   return (
     <Styled.FooterContainer>
-      <p>Copyright ©2021 Todos los derechos reservados</p>
+      <p>Copyright ©2022 Todos los derechos reservados</p>
     </Styled.FooterContainer>
   );
 }
